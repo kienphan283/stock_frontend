032e7b592aa913e19c5f61b266b6dc1541752ae1
Nusuit <huykien283@gmail.com>
2025-12-21 16:13:58 +0700

    fix: remove benchmark banner

--- src/app/(main)/stocks/[ticker]/(components)/tabs/tab-overview.tsx
+++ src/app/(main)/stocks/[ticker]/(components)/tabs/tab-overview.tsx
@@ -152,29 +152,7 @@ export default function OverviewTab({ stock }: OverviewTabProps) {
           {/* Left Column - Benchmarks + Price History (takes 3/4 width) */}
           <div className="lg:col-span-3 flex flex-col gap-6">
             {/* Benchmarks - Top Left */}
-            <Card>
-              <div className="flex justify-between items-center">
-                <div className="flex items-center gap-2">
-                  <h4 className="font-semibold text-gray-900 dark:text-white text-base">
-                    Benchmarks
-                  </h4>
-                  {/* Lock icon for unauthenticated state */}
-                  <svg
-                    className="w-4 h-4 text-gray-400"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="M12 15v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2zm10-10V7a4 4 0 00-8 0v4h8z"
-                    />
-                  </svg>
-                </div>
-              </div>
-            </Card>
+
 
             {/* Price History - Bottom Left - Flex grow to match right column height */}
             <Card className="flex-1">
